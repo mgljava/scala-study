a5744fbc79401eee2385831d5da826777e009c80
Minggl <guangliang1120@gmail.com>
2020-06-10 23:18:59 +0800

    [TECH][Minggl]: add hello world

--- src/com/github/mgljava/hello/HelloWorld.scala
+++ src/com/github/mgljava/hello/HelloWorld.scala
@@ -3,5 +3,20 @@ package com.github.mgljava.hello
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello World!")
+    welcome()
+    println(calc())
+  }
+
+  def welcome(): Unit = {
+    println("welcome")
+  }
+
+  def calc(): Int = {
+    val i = 2 * 2
+    if(i == 4) {
+      3
+    } else {
+      5
+    }
   }
 }
