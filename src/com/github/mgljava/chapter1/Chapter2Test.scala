3d6821d611caf4c03b1d360f0909e55149e0ec85
Minggl <guangliang1120@gmail.com>
2020-07-13 13:49:38 +0800

    [TECH][Monk]: scala 块表达式

--- src/com/github/mgljava/chapter1/Chapter2Test.scala
+++ src/com/github/mgljava/chapter1/Chapter2Test.scala
@@ -1,5 +1,7 @@
 package com.github.mgljava.chapter1
 
+import scala.math._
+
 object Chapter2Test {
   def main(args: Array[String]): Unit = {
     conditionExpress()
@@ -15,5 +17,27 @@ object Chapter2Test {
     var y = 0
     if (x > 3) y = 3 else y = 2
     println(y)
+
+    val r2 = if (x > 12) "hello" else 3
+    println(r2)
+
+    // 块表达式和赋值
+    val distance = {
+      val x1 = 1
+      val x2 = 2
+      val y1 = 1
+      val y2 = 2
+
+      val dx = x1 - x2
+      val dy = y1 - y2
+      sqrt(dx * dx + dy * dy) // {} 块的取值为最后一个表达式
+    }
+    println(distance)
+
+    val result = {
+      var n = 10
+      n -= 1 // 如果最后一行语句为赋值动作，那么不返回值
+    }
+    println("result: " + result) // result: ()
   }
 }
